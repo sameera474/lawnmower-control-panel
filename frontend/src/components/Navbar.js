7720f05c637c6c6a92fef6f4f21668f5de1e57c9
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-12-07 03:52:12 +0200

    update navBar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-  // Check if the admin is logged in by verifying the presence of the token
-  const isAdminLoggedIn = localStorage.getItem("adminToken");
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar
@@ -15,31 +32,72 @@ const Navbar = () => {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           SAHKA
         </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/about">
-          About
-        </Button>
-        <Button color="inherit" component={Link} to="/settings">
-          Settings
-        </Button>
-        <Button color="inherit" component={Link} to="/dashboard">
-          Dashboard
-        </Button>
-        <Button color="inherit" component={Link} to="/user-profile">
-          User Profile
-        </Button>
-        <Button color="inherit" component={Link} to="/history">
-          History
-        </Button>
 
-        {/* Conditionally render the Admin Login button */}
-        {!isAdminLoggedIn && (
-          <Button color="inherit" component={Link} to="/admin-login">
-            Admin Login
+        {/* For larger screens */}
+        <Box sx={{ display: { xs: "none", md: "block" } }}>
+          <Button color="inherit" component={Link} to="/">
+            Home
           </Button>
-        )}
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit" component={Link} to="/settings">
+            Settings
+          </Button>
+          <Button color="inherit" component={Link} to="/dashboard">
+            Dashboard
+          </Button>
+          <Button color="inherit" component={Link} to="/user-profile">
+            User Profile
+          </Button>
+          <Button color="inherit" component={Link} to="/history">
+            History
+          </Button>
+        </Box>
+
+        {/* For smaller screens */}
+        <Box sx={{ display: { xs: "block", md: "none" } }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem component={Link} to="/" onClick={handleMenuClose}>
+              Home
+            </MenuItem>
+            <MenuItem component={Link} to="/about" onClick={handleMenuClose}>
+              About
+            </MenuItem>
+            <MenuItem component={Link} to="/settings" onClick={handleMenuClose}>
+              Settings
+            </MenuItem>
+            <MenuItem
+              component={Link}
+              to="/dashboard"
+              onClick={handleMenuClose}
+            >
+              Dashboard
+            </MenuItem>
+            <MenuItem
+              component={Link}
+              to="/user-profile"
+              onClick={handleMenuClose}
+            >
+              User Profile
+            </MenuItem>
+            <MenuItem component={Link} to="/history" onClick={handleMenuClose}>
+              History
+            </MenuItem>
+          </Menu>
+        </Box>
       </Toolbar>
     </AppBar>
   );
